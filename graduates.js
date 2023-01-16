0acff266f59e71227fe5e193b9f6b5c2f23ad298
rismanharism <risman.haris6@gmail.com>
2023-01-16 21:08:16 +0700

    first commit

---
+++ graduates.js
@@ -0,0 +1,13 @@
+
+
+var nama = "risman";
+var nilai = 75;
+var absen = 6 ;
+
+// code here
+if(nilai > 75 && absen < 5){
+    console.log(nama + " dinyatakan lulus");
+}
+else{ console.log(nama + " dinyatakan tidak lulus");
+
+}
